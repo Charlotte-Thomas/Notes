15159107bb56353fdd4562de9af17268491d18be
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-03-22 16:21:29 +0000

    basic play func created

--- frontend/src/components/Create.js
+++ frontend/src/components/Create.js
@@ -6,12 +6,18 @@ import axios from 'axios'
 const Create = (props) => {
 
   const [notes, setNotes] = useState([])
+  const [block, setBlock] = useState([])
+  const [buttons, setButtons] = useState([])
+  const [songList, setSong] = useState([])
 
   useEffect(() => {
     fetch('/api/notes/')
       .then(resp => resp.json())
       .then(resp => {
         setNotes(resp)
+      })
+      .then(() => {
+        setButtons(document.querySelectorAll('.noteButton'))
       })
     return () => console.log('Unmounting component')
   }, [])
@@ -22,6 +28,45 @@ const Create = (props) => {
     audio.play()
   }
 
+  const colors = ['blue', 'pink', 'red', 'green', 'yellow']
+
+  buttons.forEach((note, i) => {
+    note.style.background = colors[i]
+    note.addEventListener('click', () => {
+      if (block) {
+        block[0].style.background = colors[i]
+        block[0].style.opacity = '1'
+        block[0].innerHTML = note.innerHTML
+        block[0].firstChild.src = note.attributes[1].value
+      }
+    })
+  })
+
+
+
+  function makeBlockChanges(id) {
+    const el = (document.getElementsByClassName('subSec')[id])
+    el.style.opacity = '0.5'
+    block.splice(0, block.length)
+    block.push(el)
+  }
+
+  function playSong() {
+    const notesToPlay = document.querySelectorAll('.subSec')
+    console.log(notesToPlay)
+    notesToPlay.forEach((n) => {
+      songList.push(n.children[1])
+    })
+    console.log(songList)
+    songList.forEach((tune, i) => {
+      setTimeout(() => {
+        tune ?  tune.play() : console.log('hi')
+      }, i * 1000)
+    })
+    songList.splice(0, songList.length)
+  }
+
+
   return (
     <div className='createPage centerCol'>
       <h1> Music Creator</h1>
@@ -30,9 +75,17 @@ const Create = (props) => {
         return <div className='noteButton centerRow' key={id} src={n.sound_file} onClick={() => playNote(id)}><p>{n.note}</p><audio className='noteAudio' src={n.sound_file}></audio></div>
       })}</div>
 
+      <button onClick={() => playSong()}>Play Song</button>
+
       <section className='player'>
-        <div className='row centerRow'><div className='subSec'></div><div className='subSec'></div><div className='subSec'></div></div>
+        <div className='row centerRow'>
+          <div className='subSec' onClick={() => makeBlockChanges(0)}><audio></audio></div>
+          <div className='subSec' onClick={() => makeBlockChanges(1)}><audio></audio></div>
+          <div className='subSec' onClick={() => makeBlockChanges(2)}><audio></audio></div>
+          <div className='subSec' onClick={() => makeBlockChanges(3)}><audio></audio></div>
+        </div>
       </section>
+      {/* <div>{block === null ? block : console.log('subsec', block)}</div> */}
 
     </div>
   )
