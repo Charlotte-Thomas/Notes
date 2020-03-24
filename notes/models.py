57a0754229ec4c7b095744d052ee8aa96c2849fd
Charlotte Thomas <charlotte.thomas64@gmail.com>
2020-03-24 21:47:42 +0000

    array of arrays for song model

--- notes/models.py
+++ notes/models.py
@@ -6,7 +6,7 @@ User = get_user_model()
 
 # Create your models here.
 class Note(models.Model):
-    note = models.CharField(max_length=200, unique=True)
+    note = models.CharField(max_length=200, unique=True, null=True)
     sound_file = models.FileField(upload_to='notes', null=True)
 
     def __str__(self):
@@ -16,7 +16,8 @@ class Song(models.Model):
     title = models.CharField(max_length=60, unique=True)
     user =  models.ForeignKey(User, related_name='notes', on_delete=models.CASCADE, null=True)
     times = ArrayField(models.CharField(max_length=200), blank=True, null=True)
-    notes = ArrayField(models.IntegerField(null=True, blank=True), null=True, blank=True)
+    notes = ArrayField(ArrayField(models.IntegerField(blank=True, null=True), blank=True, null=True), blank=True, null=True)
+    # notes = ArrayField(models.IntegerField(null=True, blank=True), null=True, blank=True)
 
     def __str__(self):
         return f'{self.title}'
